05beeedb395861f0db622763daadd1c6dba07172
astounding-spooling-2354 <nlashner@gmail.com>
2020-01-15 03:38:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function takeANumber(array, name){
   array.push(name)
   return "Welcome, " + name + ". You are number " + spot + " in line."
 }
+
+function nowServing (array){
+  if (array.length === 0){
+    return "There is no one waiting to be served!"
+  } else {
+    let person = array[0]
+    return person
+    nowServing(array.unshift())
+  }
+}

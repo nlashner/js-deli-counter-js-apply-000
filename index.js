a26f4dcab088e9cd2238cb44b613771540086e48
astounding-spooling-2354 <nlashner@gmail.com>
2020-01-15 03:39:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(array, name){
 
 function nowServing (array) {
   if (array.length === 0){
-    return "There is no one waiting to be served!"
+    return "There is nobody waiting to be served!"
   } else {
     let person = array[0]
     return person

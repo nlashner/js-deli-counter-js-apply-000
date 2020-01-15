b27dc4690833b7e78ea020fafdc86569d99e3cc5
astounding-spooling-2354 <nlashner@gmail.com>
2020-01-15 03:35:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@
 function takeANumber(array, name){
   let spot = array.length + 1
   array.push(name)
-  return "Welcome, " + name + ".  You are number " + spot + " in line."
+  return "Welcome, " + name + ". You are number " + spot + " in line."
 }

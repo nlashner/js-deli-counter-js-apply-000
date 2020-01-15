8304eb989b14c797a9550173d5f0ed727287ab7e
astounding-spooling-2354 <nlashner@gmail.com>
2020-01-15 04:15:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ function nowServing (array) {
     return `Currently serving ${person}.`
 }
 
-function currentLine (array){
-let string = "The line is currently:"
-if (array.length === 0){
-  return "The line is currently empty."
-} else {
-  for (let i = 0; i < array.length; i++){
-    let spot = i + 1
-    let name = array[i]
-    string += ` ${spot}. ${name},`
-  }
-  return string
-}
+// function currentLine (array){
+// let string = "The line is currently:"
+// if (array.length === 0){
+//   return "The line is currently empty."
+// } else {
+//   for (let i = 0; i < array.length; i++){
+//     let spot = i + 1
+//     let name = array[i]
+//     string += ` ${spot}. ${name},`
+//   }
+//   return string
+// }

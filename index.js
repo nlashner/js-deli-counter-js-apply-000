eee04a699f3d82aa804e64ba1edd8c8a6f7b69b6
astounding-spooling-2354 <nlashner@gmail.com>
2020-01-15 03:55:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing (array) {
 function currentLine (array){
 let string = "The line is currently: "
 if (array.length === 0){
-  return "The line is currently empty"
+  return "The line is currently empty."
 } else {
   for (let i = 0; i < array.length; i++){
     let spot = i + 1
